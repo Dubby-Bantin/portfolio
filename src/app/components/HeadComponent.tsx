9ad31e259af63919daefef2027de9fab43792ab3
Dubby-Bantin <dubbybantin@gmail.com>
2024-08-30 03:09:33 +0100

    added seo to the layout.tsx file

--- src/app/components/HeadComponent.tsx
+++ src/app/components/HeadComponent.tsx
@@ -2,36 +2,27 @@
 import Head from "next/head";
 import seoConfig from "../lib/seoConfig";
 
-const HeadComponent: React.FC = () => {
-  return (
-    <Head>
-      <title>{seoConfig.title}</title>
-      <meta name="description" content={seoConfig.description} />
-      <meta name="keywords" content={seoConfig.keywords} />
-      <meta property="og:type" content={seoConfig.openGraph.type} />
-      <meta property="og:locale" content={seoConfig.openGraph.locale} />
-      <meta property="og:url" content={seoConfig.openGraph.url} />
-      <meta property="og:title" content={seoConfig.openGraph.title} />
-      <meta
-        property="og:description"
-        content={seoConfig.openGraph.description}
-      />
-      <meta property="og:image" content={seoConfig.openGraph.images[0].url} />
-      <meta
-        property="og:image:width"
-        content={String(seoConfig.openGraph.images[0].width)}
-      />
-      <meta
-        property="og:image:height"
-        content={String(seoConfig.openGraph.images[0].height)}
-      />
-      <meta
-        property="og:image:alt"
-        content={seoConfig.openGraph.images[0].alt}
-      />
-      {/* Add any other meta tags or links here */}
-    </Head>
-  );
-};
+const HeadComponent: React.FC = () => (
+  <>
+    <title>{seoConfig.title}</title>
+    <meta name="description" content={seoConfig.description} />
+    <meta name="keywords" content={seoConfig.keywords} />
+    <meta property="og:type" content={seoConfig.openGraph.type} />
+    <meta property="og:locale" content={seoConfig.openGraph.locale} />
+    <meta property="og:url" content={seoConfig.openGraph.url} />
+    <meta property="og:title" content={seoConfig.openGraph.title} />
+    <meta property="og:description" content={seoConfig.openGraph.description} />
+    <meta property="og:image" content={seoConfig.openGraph.images[0].url} />
+    <meta
+      property="og:image:width"
+      content={String(seoConfig.openGraph.images[0].width)}
+    />
+    <meta
+      property="og:image:height"
+      content={String(seoConfig.openGraph.images[0].height)}
+    />
+    <meta property="og:image:alt" content={seoConfig.openGraph.images[0].alt} />
+  </>
+);
 
 export default HeadComponent;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import { ThemeProvider } from "next-themes";
 import seoConfig from "./lib/seoConfig";
-
-const inter = Inter({ subsets: ["latin"] });
+import HeadComponent from "./components/HeadComponent";
 
 export const metadata: Metadata = {
   title: {
@@ -20,30 +18,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <title>{seoConfig.title}</title>
-      <meta name="description" content={seoConfig.description} />
-      <meta name="keywords" content={seoConfig.keywords} />
-      <meta property="og:type" content={seoConfig.openGraph.type} />
-      <meta property="og:locale" content={seoConfig.openGraph.locale} />
-      <meta property="og:url" content={seoConfig.openGraph.url} />
-      <meta property="og:title" content={seoConfig.openGraph.title} />
-      <meta
-        property="og:description"
-        content={seoConfig.openGraph.description}
-      />
-      <meta property="og:image" content={seoConfig.openGraph.images[0].url} />
-      <meta
-        property="og:image:width"
-        content={String(seoConfig.openGraph.images[0].width)}
-      />
-      <meta
-        property="og:image:height"
-        content={String(seoConfig.openGraph.images[0].height)}
-      />
-      <meta
-        property="og:image:alt"
-        content={seoConfig.openGraph.images[0].alt}
-      />
+      <HeadComponent />
       <body>
         <ThemeProvider
           attribute="class"
